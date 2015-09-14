0591278648bfd53e7de58242fccd070c854a0cec
Jacob Wahlgren <jacob.wahlgren@gmail.com>
2015-09-14 22:06:50 +0200

    Generate URL from object

--- app/app.js
+++ app/app.js
@@ -1,11 +1,14 @@
-// Browser modules are imported through new ES6 syntax.
-//import loadSchedule from './schedule_loader/loader.js';
+import * as schedule from './schedule/schedule.js';
 
 // window.env contains data from config/env_XXX.json file.
-var envName = window.env.name;
+// var envName = window.env.name;
 
-const url = 'http://www.novasoftware.se/ImgGen/schedulegenerator.aspx?format=png&schoolid=29120/sv-se&type=-1&id=es13a&period=&week=38&mode=0&printer=0&colors=32&head=0&clock=0&foot=0&day=0&width=1222&height=415&maxwidth=1222&maxheight=415';
+const url = schedule.url({
+    school: schedule.schools['Globala gymnasiet'],
+    id: 'es13a',
+    week: 38,
+    width: 600,
+    height: 400
+});
 
 document.getElementById('schedule-img').src = url;
-
-console.log("hi!");
--- app/schedule/schedule.js
+++ app/schedule/schedule.js
@@ -1,4 +1,31 @@
 'use strict';
 
-export default function(url) {
+const baseUrl = 'http://www.novasoftware.se/ImgGen/schedulegenerator.aspx';
+
+export const schools = {
+    'Globala gymnasiet': 29120
+};
+
+export function url(schedule) {
+    return baseUrl.concat(
+        '?',
+        'format=', 'png',
+        '&schoolid=', schedule.school,
+        '/sv-se',
+        '&type=-1',
+        '&id=', schedule.id,
+        '&period=',
+        '&week=', schedule.week,
+        '&mode=0',
+        '&printer=0',
+        '&colors=32',
+        '&head=0',
+        '&clock=0',
+        '&foot=0',
+        '&day=0',
+        '&width=', schedule.width,
+        '&height=', schedule.height,
+        '&maxwidth=', schedule.width,
+        '&maxheight=', schedule.height
+    );
 }
