5ef20d9f23063afd33d0b1b362c799f2e33995a1
Jacob Wahlgren <jacob.wahlgren@gmail.com>
2015-09-20 00:46:48 +0200

    Use util.log in main process
    Includes timestamps.

--- app/main/messaging.js
+++ app/main/messaging.js
@@ -3,10 +3,11 @@
 var app = require('app');
 var BrowserWindow = require('browser-window');
 var ipc = require('ipc');
+var util = require('util');
 var config = require('./config');
 
 ipc.on('load-schedule', function(event) {
-    console.log('Recieved load-schedule request');
+    util.log('Recieved load-schedule request');
 
     config.loadSchedule()
     .then(function(schedule) {
@@ -18,13 +19,13 @@ ipc.on('load-schedule', function(event) {
 });
 
 ipc.on('save-schedule', function(event, schedule) {
-    console.log('Recieved save-schedule request');
+    util.log('Recieved save-schedule request');
 
     config.saveSchedule(schedule);
 });
 
 ipc.on('reload', function (event) {
-    console.log('Recieved reload request');
+    util.log('Recieved reload request');
 
     BrowserWindow.getFocusedWindow().reloadIgnoringCache();
 });
