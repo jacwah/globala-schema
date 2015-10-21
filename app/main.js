e76ba1b963ce02810e51726012d8870179abf469
Jacob Wahlgren <jacob.wahlgren@gmail.com>
2015-10-21 22:59:20 +0200

    Improve menus and setup for About page

--- app/main.js
+++ app/main.js
@@ -3,11 +3,12 @@
 var app = require('app');
 var BrowserWindow = require('browser-window');
 var env = require('./vendor/electron_boilerplate/env_config');
-var devHelper = require('./vendor/electron_boilerplate/dev_helper');
+var Menu = require('menu');
 var windowStateKeeper = require('./vendor/electron_boilerplate/window_state');
 
-
-var mainWindow;
+// Keep windows from being GC'd
+var mainWindow = null;
+var aboutWindow = null;
 
 // Preserver of the window size and position between app launches.
 var mainWindowState = windowStateKeeper('main', {
@@ -15,9 +16,89 @@ var mainWindowState = windowStateKeeper('main', {
     height: 600
 });
 
-app.on('ready', function () {
+function openAbout() {
+    if (aboutWindow == null) {
+        aboutWindow = new BrowserWindow({
+            center: true,
+            resizeable: false
+        });
 
+        aboutWindow.on('close', function() {
+            aboutWindow = null;
+        });
+
+        aboutWindow.loadUrl('file://' + __dirname + '/about.html');
+    }
+}
+
+function createMenus() {
+    var name = app.getName();
+    var template = [
+        {
+            label: name,
+            submenu: [
+                {
+                    label: 'About ' + name,
+                    click: function() { openAbout(); }
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    label: 'Hide ' + name,
+                    accelerator: 'Command+H',
+                    role: 'hide'
+                },
+                {
+                    label: 'Hide Others',
+                    accelerator: 'Command+Shift+H',
+                    role: 'hideothers'
+                },
+                {
+                    label: 'Show all',
+                    role: 'unhide'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    label: 'Quit',
+                    accelerator: 'Command+Q',
+                    click: function() { app.quit(); }
+                }
+            ]
+        }
+    ];
+
+    if (env.name == 'development') {
+        template.push({
+            label: 'Development',
+            submenu: [
+                {
+                    label: 'Reload',
+                    accelerator: 'CmdOrCtrl+R',
+                    click: function () {
+                        BrowserWindow.getFocusedWindow().reloadIgnoringCache();
+                    }
+                },
+                {
+                    label: 'Toggle Dev Tools',
+                    accelerator: 'Alt+CmdOrCtrl+I',
+                    click: function () {
+                        BrowserWindow.getFocusedWindow().toggleDevTools();
+                    }
+                }
+            ]
+        });
+    }
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+}
+
+app.on('ready', function () {
     require('./main/messaging');
+
+    createMenus();
 
     mainWindow = new BrowserWindow({
         x: mainWindowState.x,
@@ -31,10 +112,6 @@ app.on('ready', function () {
     }
 
     mainWindow.loadUrl('file://' + __dirname + '/app.html');
-
-    if (env.name === 'development') {
-        devHelper.setDevMenu();
-    }
 
     mainWindow.on('close', function () {
         mainWindowState.saveState(mainWindow);
