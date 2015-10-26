428d8a777410fa20491ec604646f993270854f8b
Jacob Wahlgren <jacob.wahlgren@gmail.com>
2015-10-27 00:26:29 +0100

    Use real ids and generate url

--- app/render/index.js
+++ app/render/index.js
@@ -1,15 +1,27 @@
 'use strict';
 
 angular.module('scheduleApp', [])
-    .controller('SelectionController', function() {
-        console.log('selection');
+    .controller('SelectionController', ['currentSchedule', 'Schedule', function(currentSchedule, Schedule) {
         var selection = this;
 
-        selection.ids = [1, 2, 3, 4, 5];
+        selection.ids = Schedule.ids;
         selection.current = selection.ids[0];
+
+        selection.currentSchedule = currentSchedule;
 
         selection.set = function(id) {
             selection.current = id;
+            currentSchedule.set(id);
         }
-    })
+    }])
+    .constant('Schedule', require('./model/schedule'))
+    .service('currentSchedule', ['Schedule', function(Schedule) {
+        this.url = Schedule.url(Schedule.defaults());
+
+        this.set = function(id) {
+            var schedule = Schedule.defaults();
+            schedule.id = id;
+            this.url = Schedule.url(schedule);
+        };
+    }])
 ;
