3b6bd2e6ea70d31001e319dfb2dba604a788c5b1
Jacob Wahlgren <jacob.wahlgren@gmail.com>
2015-10-28 01:14:05 +0100

    Refactor controller and service

--- app/render/index.js
+++ app/render/index.js
@@ -12,23 +12,19 @@ angular.module('scheduleApp', [])
         var selection = this;
 
         selection.ids = Schedule.ids;
-        selection.current = selection.ids[0];
-
-        selection.currentSchedule = currentSchedule;
-
-        selection.set = function(id) {
-            selection.current = id;
-            currentSchedule.set(id);
-        }
+        selection.current = currentSchedule;
+        selection.current.setId(selection.ids[0]);
     }])
     .constant('Schedule', require('./model/schedule'))
     .service('currentSchedule', ['Schedule', function(Schedule) {
         this.obj = Schedule.defaults();
-        this.url = Schedule.url(this.obj);
 
-        this.set = function(id) {
+        this.setId = function(id) {
             this.obj.id = id;
-            this.url = Schedule.url(this.obj);
+        };
+
+        this.getUrl = function() {
+            return Schedule.url(this.obj);
         };
     }])
 ;
